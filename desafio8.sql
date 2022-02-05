9e81d362fea95f673c1886ea3ba878a5d88fa1dd
Wilian Pereira <wilian_pereira92@outlook.com>
2022-02-04 21:19:39 -0300

    Adicionando Requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,7 @@
+SELECT 
+	ar.nome AS artista,
+    al.nome AS album
+FROM SpotifyClone.artista AS ar
+INNER JOIN SpotifyClone.album AS al
+ON ar.id = al.artista_id
+WHERE ar.nome = 'Walter Phoenix';
